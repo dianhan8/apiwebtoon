bb53caa722fd9cc5459ae324fa54a785b97ea463
Dian <dianhan8@gmail.com>
2019-10-24 20:24:37 +0700

    ADD WEBTOON

--- controllers/episode.js
+++ controllers/episode.js
@@ -1,5 +1,9 @@
 const Episode = require('../models').episode
 const Webtoon = require('../models').webtoon
+const Detail = require('../models').detail
+
+const multer = require('multer');
+const path = require('path');
 
 //Get All Episode By Webtoon Id
 exports.show = (req, res) => {
@@ -41,27 +45,109 @@ exports.showEpisodeById = (req, res) => {
         })
 }
 
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + req.user.userId + '-' + Date.now() + path.extname(file.originalname));
+    }
+})
+
+// Init Upload
+const uploadEpisode = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('cover');
+// Init Upload
+const uploadDetailEpisode = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).array('image');
+
+// Check File Type
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
 //Create Episode By Webtoon
 exports.storeEpisode = (req, res) => {
-    const webtoon_id = req.params.webtoonid
-    const body = req.body
-    Episode.create({
-        title: body.title,
-        cover: body.cover,
-        webtoon_id,
-        createdAt: new Date(),
-        updatedAt: new Date()
-    })
-        .then(function (result) {
-            res.send(result)
-        })
-        .catch(function (err) {
+    uploadEpisode(req, res, (err) => {
+        if (err) {
             res.send({
-                error: true,
-                message: "Can't Create Webtoon",
-                err
-            })
-        })
+                msg: err
+            });
+        } else {
+            if (req.file == undefined) {
+                res.send({
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                res.send({
+                    msg: 'File Uploaded!',
+                    dest: req.file.destination,
+                })
+                Episode.create({
+                    title: req.body.title,
+                    cover: req.file.path,
+                    webtoon_id: req.body.webtoon_id,
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                })
+                    .then(function (result) {
+                        res.send(result)
+                        const id = result.id
+                        uploadDetailEpisode(req, res, (err) => {
+                            if (err) {
+                                res.send({
+                                    msg: err
+                                });
+                            } else {
+                                if (req.file == undefined) {
+                                    res.send({
+                                        msg: 'Error: No File Selected!'
+                                    });
+                                } else {
+                                    res.send({
+                                        msg: 'File Uploaded!',
+                                        dest: req.file.destination,
+                                    })
+                                    Detail.bulkCreate(req.body.detail)
+                                        .then(function (rest) {
+                                            res.send(rest)
+                                        })
+                                        .catch(function (err) {
+                                            res.send(err)
+                                        })
+                                }
+                            }
+                        })
+                    })
+                    .catch(function (err) {
+                        res.send({
+                            error: true,
+                            message: "Can't Create Webtoon",
+                            err
+                        })
+                    })
+            }
+        }
+    })
 }
 
 //Update Episode by Id
--- controllers/webtoon.js
+++ controllers/webtoon.js
@@ -2,6 +2,9 @@ const Webtoon = require('../models').webtoon
 const Users = require('../models').user
 const Op = require('sequelize').Op
 
+const multer = require('multer');
+const path = require('path');
+
 
 //Method Get /webtoons && /webtoons?title
 exports.index = (req, res) => {
@@ -81,83 +84,133 @@ exports.showByUsers = (req, res) => {
         })
 }
 
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + req.user.userId + '-' + Date.now() + path.extname(file.originalname));
+    }
+})
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('image');
+
+// Check File Type
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
 //Store /Create Webtoons By Users Id
 exports.store = (req, res) => {
-    const user_id = req.params.id
-    const body = req.body
-    Webtoon.create({
-        title: body.title,
-        genre: body.genre,
-        cover: body.cover,
-        isFavorite: false,
-        user_id,
-        createdAt: new Date(),
-        updatedAt: new Date()
+    upload(req, res, (err) => {
+        if (err) {
+            res.send({
+                msg: err
+            });
+        } else {
+            if (req.file == undefined) {
+                res.send({
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                res.send({
+                    msg: 'File Uploaded!',
+                    dest: req.file.destination,
+                })
+                Webtoon.create({
+                    title: req.body.title,
+                    genre: req.body.genre,
+                    cover: req.file.path,
+                    isFavorite: false,
+                    user_id: req.user.userId,
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                })
+                    .then(function (result) {
+                        res.send({
+                            message: "Your Account Has Created"
+                        })
+                    })
+                    .catch(function (err) {
+                        res.send({
+                            error: true,
+                            message: "Error",
+                            err
+                        })
+                    })
+            }
+        }
     })
-        .then(function (result) {
-            res.send({
-                message: "Your Account Has Created"
-            })
-        })
-        .catch(function (err) {
-            res.send({
-                error: true,
-                message: "Error",
-                err
-            })
-        })
 }
 
 
 
 // UpdateBy Webtoons
 exports.UpdateByWebtoons = (req, res) => {
-    const id = req.params.webtoonid
-    const body = req.body
-    Webtoon.update({
-        title: body.title,
-        genre: body.genre,
-        cover: body.cover,
-        updatedAt: new Date()
-    },
-        {
-            where: { id }
-        })
-        .then(function (result) {
-            res.send({
-                ...body,
-                result
-            })
-        })
-        .catch(function (err) {
-            res.send({
-                error: true,
-                message: "Error can't update",
-                err
-            })
-        })
-}
+            const id = req.params.webtoonid
+            const body = req.body
+            Webtoon.update({
+                title: body.title,
+                genre: body.genre,
+                cover: body.cover,
+                updatedAt: new Date()
+            },
+                {
+                    where: { id }
+                })
+                .then(function (result) {
+                    res.send({
+                        ...body,
+                        result
+                    })
+                })
+                .catch(function (err) {
+                    res.send({
+                        error: true,
+                        message: "Error can't update",
+                        err
+                    })
+                })
+        }
 
 //Delete Webtoon By Users
 exports.DeleteByWebtoons = (req, res) => {
-    const id = req.params.webtoonid
-    Webtoon.destroy({
-        where: { id }
-    })
-        .then(function (result) {
-            res.send({
-                id,
-                msg: `Webtoons Id ${id} has deleted`
+            const id = req.params.webtoonid
+            Webtoon.destroy({
+                where: { id }
             })
-        })
-        .catch(function (err) {
-            res.send({
-                error: true,
-                message: "Can't deleted webtoon",
-                err
-            })
-        })
-}
+                .then(function (result) {
+                    res.send({
+                        id,
+                        msg: `Webtoons Id ${id} has deleted`
+                    })
+                })
+                .catch(function (err) {
+                    res.send({
+                        error: true,
+                        message: "Can't deleted webtoon",
+                        err
+                    })
+                })
+        }
 
 
 
